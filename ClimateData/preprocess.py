3ab53dbe6ad529e7502be51f5bd48b1ef0e980cf
Maxwell Carlson <bart4282819@gmail.com>
2022-01-28 22:58:46 -0800

    have files without their identifier embeded inside the first column now.

--- ClimateData/preprocess.py
+++ ClimateData/preprocess.py
@@ -9,12 +9,13 @@ filesToStrip = ['avgtmp', 'maxtmp', 'mintmp', 'precip']
 
 if __name__ == '__main__':
     cols = [i for i in range(13)]
-    dtypes = [str] + [np.float] * 12
-    d = pd.DataFrame(np.vstack([cols, dtypes])).to_dict(orient='records')
+    dtypes = [str] + [str] * 12
+    d = pd.DataFrame(np.vstack([cols, dtypes])).to_dict(orient='records')[1]
 
     for filename in filesToStrip:
-        df = pd.read_csv(f'{datadir}climdiv-{filename}.csv', delimiter='  ', header=None, index_col=False)#, usecols=cols, dtype=d[1])
+        df = pd.read_csv(f'{datadir}climdiv-{filename}.csv', delimiter='  ', header=None, index_col=False, usecols=cols, dtype=d)
         
+        # Remove datatype field, since it's the same throughout the entirity of each file
         s = df.iloc[:,0]
         s = s.str[0:5] + s.str[7:]
         df.iloc[:,0] = s
