678a32c8afcd5dcf468549232686dac33d19bc9d
Melissa Barnes <barnes28@pdx.edu>
2022-02-06 10:46:31 -0800

    Database is now filled by iterating over processed files

--- ClimateData/database.py
+++ ClimateData/database.py
@@ -5,40 +5,8 @@ from os import listdir
 import json
 from psycopg2.extensions import AsIs
 
+
 outputDir = './data/processed/'
-
-def setup_database_old():
-    conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=PASSWORD")
-    cur = conn.cursor()
-
-    tableName  = os.path.basename('county_codes.csv').split(".")[0]
-    print(tableName)
-    with open(f'{outputDir}county_codes.csv', 'r', encoding='utf-8-sig') as f:
-            reader = csv.reader(f)
-            columns = next(reader)
-    column_string = ", ".join(columns)
-    print(column_string)
-
-    cur.execute("""
-        CREATE TABLE %s(
-        %s)
-        """,
-        [AsIs(tableName), AsIs(column_string),])
-
-    conn.commit()
-
-    with open(f'{outputDir}county_codes.csv', 'r', encoding='utf-8-sig') as f:
-        next(f) # Skip the header row.
-        cur.copy_from(f, f'{tableName}', sep=',')
-    conn.commit()
-
-def load_data():
-    conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=PASSWORD")
-    cur = conn.cursor()
-    with open('./data/processed/complete.csv', 'r') as f:
-        next(f) # Skip the header row.
-        cur.copy_from(f, 'climate_data', sep=',')
-    conn.commit()
 
 
 def setup_database():
@@ -53,19 +21,21 @@ def setup_database():
             columns = next(reader)
         column_string = ", ".join(columns)
         print(column_string)
-        #conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=PASSWORD")
-        #cur = conn.cursor()
-        #cur.execute("""
-        #CREATE TABLE %s(
-        #%s)
-        #""",
-        #[AsIs(tableName), AsIs(column_string),])
-        #conn.commit()
+        
+        conn = psycopg2.connect("host=localhost dbname=postgres user=postgres password=PASSWORD")
+        cur = conn.cursor()
+        cur.execute("""
+        CREATE TABLE %s(
+        %s)
+        """,
+        [AsIs(tableName), AsIs(column_string),])
+        print(cur.rowcount)
+        conn.commit()
 
-        #with open(f'{outputDir}{fileName}', 'r', encoding='utf-8-sig') as f:
-        #    next(f) # Skip the header row.
-        #    cur.copy_from(f, f'{tableName}', sep=',')
-        #conn.commit()
+        with open(f'{outputDir}{fileName}', 'r', encoding='utf-8-sig') as f:
+            next(f) # Skip the header row.
+            cur.copy_from(f, f'{tableName}', sep=',')
+        conn.commit()
 
 def find_csv_filenames( path_to_dir, suffix=".csv" ):
     filenames = listdir(path_to_dir)
