873c6ed2d13043740458d9de210ab8e4668d227a
Melissa Barnes <barnes28@pdx.edu>
2022-02-14 17:29:59 -0800

    cleaning up

--- ClimateData/database.py
+++ ClimateData/database.py
@@ -180,9 +180,3 @@ def get_data(tableName, columnList, idList, startYear, endYear):
 
 setup_database()
 
-tableName = "weather"
-columnList = ["id", "tmp_avg_jan"]
-idList = ["0101001", "0101005"]
-startYear = 1990
-endYear = 1995
-#get_data(tableName, columnList, idList, startYear, endYear)
