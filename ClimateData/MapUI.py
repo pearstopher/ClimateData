227cd882cf7f81a614a4a4e5b9be4b0c70421dd6
Mark Montes <mark.montes44@gmail.com>
2022-02-20 14:17:15 -0800

    added different maplayouts and date drop down

--- ClimateData/MapUI.py
+++ ClimateData/MapUI.py
@@ -1,6 +1,7 @@
 from cgitb import text
 from tkinter import *
 from tkinter import ttk
+from tkinter.tix import ButtonBox
 from turtle import left
 from tkintermapview import TkinterMapView as TKMV
 import psycopg2
@@ -17,7 +18,7 @@ buttonFrame.pack()
 mapFrame = Frame(root)
 mapFrame.pack(side=BOTTOM)
 rightFrame = Frame(root)
-rightFrame.pack(side=RIGHT)
+rightFrame.pack(side=BOTTOM)
 
 states_abb = {
     "Alabama": "AL",
@@ -101,6 +102,17 @@ dropdown['values'] = (['Alaska','Alabama','Arkansas','Arizona','California','Col
 dropdown.bind('<<ComboboxSelected>>', SelectState)
 dropdown.pack(pady=10)
 
+#Month + Year Drop down
+start_date = Label(buttonFrame, text="Start Date")
+start_date.pack()
+start_month = ttk.Combobox(buttonFrame)
+start_month.pack(side=LEFT)
+# month_dropdown['month'] = 'readonly'
+start_month['values'] = (['01', '02', '03', '04', '05', '06', '07', '08', '09', '10', '11', '12'])
+
+start_year = ttk.Combobox(buttonFrame)
+start_year.pack(side=LEFT)
+
 #Right Frame
 clearButton = Button(rightFrame, text="Clear all marks", command=clearMarks)
 clearButton.pack(expand=True)
@@ -109,5 +121,9 @@ map_widget = TKMV(root, width=800, height=600, corner_radius=100)
 map_widget.place(relx=0.5, rely=0.5, anchor=CENTER)
 map_widget.set_address("Colorado")
 map_widget.set_zoom(4)
+map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=m&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google normal
+#map_widget.set_tile_server("https://mt0.google.com/vt/lyrs=s&hl=en&x={x}&y={y}&z={z}&s=Ga", max_zoom=22)  # google satellite
+#map_widget.set_tile_server("http://c.tile.stamen.com/watercolor/{z}/{x}/{y}.png")  # painting style
+#map_widget.set_tile_server("http://a.tile.stamen.com/toner/{z}/{x}/{y}.png")  # black and white
 
 root.mainloop()
