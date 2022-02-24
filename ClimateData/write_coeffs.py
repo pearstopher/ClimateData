db9c1e25579b0b1091ff453186139e2df6f568b4
ericanderson333 <ericsanderson333@gmail.com>
2022-02-23 16:15:25 -0800

    Finished building csv for coeffs

--- ClimateData/write_coeffs.py
+++ ClimateData/write_coeffs.py
@@ -1,38 +1,26 @@
-import numpy as np
-import matplotlib.pyplot as plt
 import numpy.polynomial.polynomial as poly
-import pandas as pd
-import pandas.io.sql as pdsql
 from database import *
 import psycopg2
-import csv
-from psycopg2.extensions import AsIs
 
-'''
-interact with db
-get polynomial
-get derivative
-write to csv
-'''
 # Predefined lists
-start_year = '1895'
-end_year = '2021'
-country = 'US'
-states = [ 'AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
-           'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
-           'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
-           'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
-           'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
-# temp_avg_cols = ['tmp_avg_jan', 'tmp_avg_feb', 'tmp_avg_mar', 'tmp_avg_apr', 'tmp_avg_jun', 'tmp_avg_jul',
-#                  'tmp_avg_aug', 'tmp_avg_sep', 'tmp_avg_oct', 'tmp_avg_nov', 'tmp_avg_dec']
-# temp_max_cols = ['tmp_max_jan', 'tmp_max_feb', 'tmp_max_mar', 'tmp_max_apr', 'tmp_max_jun', 'tmp_max_jul',
-#                  'tmp_max_aug', 'tmp_max_sep', 'tmp_max_oct', 'tmp_max_nov', 'tmp_max_dec']
-# temp_min_cols = ['tmp_min_jan', 'tmp_min_feb', 'tmp_min_mar', 'tmp_min_apr', 'tmp_min_jun', 'tmp_min_jul',
-#                  'tmp_min_aug', 'tmp_min_sep', 'tmp_min_oct', 'tmp_min_nov', 'tmp_min_dec']
-# precip_cols = ['precip_jan', 'precip_feb', 'precip_mar', 'precip_apr', 'precip_jun', 'precip_jul',
-#                  'precip_aug', 'precip_sep', 'precip_oct', 'precip_nov', 'precip_dec']
+states = ['AK', 'AL', 'AR', 'AZ', 'CA', 'CO', 'CT', 'DC', 'DE', 'FL', 'GA',
+          'HI', 'IA', 'ID', 'IL', 'IN', 'KS', 'KY', 'LA', 'MA', 'MD', 'ME',
+          'MI', 'MN', 'MO', 'MS', 'MT', 'NC', 'ND', 'NE', 'NH', 'NJ', 'NM',
+          'NV', 'NY', 'OH', 'OK', 'OR', 'PA', 'RI', 'SC', 'SD', 'TN', 'TX',
+          'UT', 'VA', 'VT', 'WA', 'WI', 'WV', 'WY']
+temp_avg_cols = ['id', 'tmp_avg_jan', 'tmp_avg_feb', 'tmp_avg_mar', 'tmp_avg_apr', 'tmp_avg_may', 'tmp_avg_jun',
+                 'tmp_avg_jul',
+                 'tmp_avg_aug', 'tmp_avg_sep', 'tmp_avg_oct', 'tmp_avg_nov', 'tmp_avg_dec']
+temp_max_cols = ['id', 'tmp_max_jan', 'tmp_max_feb', 'tmp_max_mar', 'tmp_max_apr', 'tmp_max_may', 'tmp_max_jun',
+                 'tmp_max_jul',
+                 'tmp_max_aug', 'tmp_max_sep', 'tmp_max_oct', 'tmp_max_nov', 'tmp_max_dec']
+temp_min_cols = ['id', 'tmp_min_jan', 'tmp_min_feb', 'tmp_min_mar', 'tmp_min_apr', 'tmp_min_may', 'tmp_min_jun',
+                 'tmp_min_jul',
+                 'tmp_min_aug', 'tmp_min_sep', 'tmp_min_oct', 'tmp_min_nov', 'tmp_min_dec']
+precip_cols = ['id', 'precip_jan', 'precip_feb', 'precip_mar', 'precip_apr', 'precip_may', 'precip_jun', 'precip_jul',
+               'precip_aug', 'precip_sep', 'precip_oct', 'precip_nov', 'precip_dec']
 
-
+''' Query to get list of ids, county codes, county names, states, and countries from DB'''
 def get_county_codes_as_df():
     cols = ['id', 'county_code', 'county_name', 'state', 'country']
     conn = psycopg2.connect(f"host=localhost dbname=postgres user=postgres password=PASSWORD")
@@ -45,10 +33,136 @@ def get_county_codes_as_df():
     return res
 
 
-df = get_county_codes_as_df()
-state_dict_df = {}
-for state in states:
-    state_dict_df[state] = df[(df['state'] == state)]
+''' Wrapper to get weather data split into their respective df (avg, max, min, precip) '''
+def get_data_dfs(row):
+    # Could use state from for loop...
+    # However, want to stay consistent with state recorded in db record
+    start_year = 1895
+    end_year = 2021
+    country = 'US'
+    county = row['county_name']
+    state = row['state']
 
-print(state_dict_df)
-# iterate rows while on col (state) val?
+    # DB call -> Returns pd.Dataframe
+    avg_data_df = get_data_for_single_county(temp_avg_cols, county, state, country, start_year, end_year)
+    max_data_df = get_data_for_single_county(temp_max_cols, county, state, country, start_year, end_year)
+    min_data_df = get_data_for_single_county(temp_min_cols, county, state, country, start_year, end_year)
+    precip_data_df = get_data_for_single_county(precip_cols, county, state, country, start_year, end_year)
+
+    return [avg_data_df, max_data_df, min_data_df, precip_data_df]
+
+
+''' Get data in x & y format per df '''
+def get_xy_data(df):
+    x_dates_format = []
+    x_data = []
+    start_year = int(str(df['id'].iloc[0])[6:])
+    end_year = int(str(df['id'].iloc[-1])[6:])
+    # for i in df['id']:
+    for i in range(start_year, end_year + 1):
+        for j in range(1, 13):
+            x_dates_format.append(str(i)[-4:] + '-' + str(j))
+            x_data.append(int(str(i)[-4:]) + (j - 1) / 12)
+
+    y_data = []
+    # for i, row in df.head(127).iterrows():
+    for i, row in df.iterrows():
+        for j in row[1:13]:
+            y_data.append(j)
+
+    return [x_data, y_data, x_dates_format]
+
+
+''' Builds csv for county polynomial coefficents '''
+def build_poly_coeffs_for_county_csv(deg):
+    print(f'Degree {deg} Polynomial')
+
+    cols = []
+    if deg == 4:
+        cols = ['State', 'County', 'a', 'b', 'c', 'd', 'e']
+    if deg == 3:
+        cols = ['State', 'County', 'a', 'b', 'c', 'd']
+    if deg == 2:
+        cols = ['State', 'County', 'a', 'b', 'c']
+    if deg == 1:
+        cols = ['State', 'County', 'a', 'b']
+
+    assert len(cols) > 0
+
+    avg_data_rows = []
+    max_data_rows = []
+    min_data_rows = []
+    precip_data_rows = []
+
+    for state in states:
+        print(f"Storing sub-dataframe for state: {state}")
+
+        county_codes_df = get_county_codes_as_df()
+        state_df = county_codes_df[(county_codes_df['state'] == state)]
+        state_df.reset_index()  # make sure indices pair with number of rows
+        for index, row in state_df.iterrows():
+            print(f"Building and storing coefficients for county: {row['county_name']}")
+            [avg_df, max_df, min_df, precip_df] = get_data_dfs(row)
+
+            if avg_df.empty is True and max_df.empty is True and min_df.empty is True and precip_df.empty is True:
+                continue
+
+            [x_avg, y_avg, x_avg_dates] = get_xy_data(avg_df)
+            [x_max, y_max, x_max_dates] = get_xy_data(max_df)
+            [x_min, y_min, x_min_dates] = get_xy_data(min_df)
+            [x_precip, y_precip, x_precip_dates] = get_xy_data(precip_df)
+
+            avg_coeffs = poly.polyfit(x_avg, y_avg, deg)
+            max_coeffs = poly.polyfit(x_max, y_max, deg)
+            min_coeffs = poly.polyfit(x_min, y_min, deg)
+            precip_coeffs = poly.polyfit(x_precip, y_precip, deg)
+
+            if deg == 4:
+                avg_data_rows.append(
+                    [row['state'], row['county_name'], avg_coeffs[0], avg_coeffs[1], avg_coeffs[2], avg_coeffs[3],
+                     avg_coeffs[4]])
+                max_data_rows.append(
+                    [row['state'], row['county_name'], max_coeffs[0], max_coeffs[1], max_coeffs[2], max_coeffs[3],
+                     max_coeffs[4]])
+                min_data_rows.append(
+                    [row['state'], row['county_name'], min_coeffs[0], min_coeffs[1], min_coeffs[2], min_coeffs[3],
+                     min_coeffs[4]])
+                precip_data_rows.append(
+                    [row['state'], row['county_name'], precip_coeffs[0], precip_coeffs[1], precip_coeffs[2],
+                     precip_coeffs[3], precip_coeffs[4]])
+            if deg == 3:
+                avg_data_rows.append(
+                    [row['state'], row['county_name'], avg_coeffs[0], avg_coeffs[1], avg_coeffs[2], avg_coeffs[3]])
+                max_data_rows.append(
+                    [row['state'], row['county_name'], max_coeffs[0], max_coeffs[1], max_coeffs[2], max_coeffs[3]])
+                min_data_rows.append(
+                    [row['state'], row['county_name'], min_coeffs[0], min_coeffs[1], min_coeffs[2], min_coeffs[3]])
+                precip_data_rows.append(
+                    [row['state'], row['county_name'], precip_coeffs[0], precip_coeffs[1], precip_coeffs[2],
+                     precip_coeffs[3]])
+            if deg == 2:
+                avg_data_rows.append([row['state'], row['county_name'], avg_coeffs[0], avg_coeffs[1], avg_coeffs[2]])
+                max_data_rows.append([row['state'], row['county_name'], max_coeffs[0], max_coeffs[1], max_coeffs[2]])
+                min_data_rows.append([row['state'], row['county_name'], min_coeffs[0], min_coeffs[1], min_coeffs[2]])
+                precip_data_rows.append(
+                    [row['state'], row['county_name'], precip_coeffs[0], precip_coeffs[1], precip_coeffs[2]])
+            if deg == 1:
+                avg_data_rows.append([row['state'], row['county_name'], avg_coeffs[0], avg_coeffs[1]])
+                max_data_rows.append([row['state'], row['county_name'], max_coeffs[0], max_coeffs[1]])
+                min_data_rows.append([row['state'], row['county_name'], min_coeffs[0], min_coeffs[1]])
+                precip_data_rows.append([row['state'], row['county_name'], precip_coeffs[0], precip_coeffs[1]])
+
+    avg_poly_df = pd.DataFrame(avg_data_rows, columns=cols)
+    max_poly_df = pd.DataFrame(max_data_rows, columns=cols)
+    min_poly_df = pd.DataFrame(min_data_rows, columns=cols)
+    precip_poly_df = pd.DataFrame(precip_data_rows, columns=cols)
+
+    avg_poly_df.to_csv(f"{deg}_avg_county_coeffs.csv", sep=',', encoding='utf-8', index=False)
+    max_poly_df.to_csv(f"{deg}_max_poly_test.csv", sep=',', encoding='utf-8', index=False)
+    min_poly_df.to_csv(f"{deg}_min_poly_test.csv", sep=',', encoding='utf-8', index=False)
+    precip_poly_df.to_csv(f"{deg}_precip_poly_test.csv", sep=',', encoding='utf-8', index=False)
+    print(f'Successfully wrote polynomial coeffs to csv!')
+
+
+if __name__ == '__main__':
+    build_poly_coeffs_for_county_csv(3)
