379dcb00a7a39d1c90b0133fc0fec6eeb531fc9a
Mark Montes <mark.montes44@gmail.com>
2022-04-07 21:20:48 -0700

    merge issue with latest develop

--- ClimateData/plotting.py
+++ ClimateData/plotting.py
@@ -1,10 +1,19 @@
+import matplotlib
 import numpy as np
 import matplotlib.pyplot as plt
+from matplotlib.backends.backend_tkagg import (FigureCanvasTkAgg,
+NavigationToolbar2Tk)
+import matplotlib.dates as mdates
 import numpy.polynomial.polynomial as poly
 import pandas as pd
 import mplcursors
 from string import ascii_lowercase
+from tkinter import *
+from matplotlib.pyplot import cm
+import math
+import datetime as dt
 
+matplotlib.use("TkAgg")
 '''
 TODO
 functions to implement
@@ -42,58 +51,81 @@ def get_test_data_raw():
     df.columns = headers
     return df
 
-def plot(ptype, df, plot_vars_map):
+def to_date(x_data):
+    x_data_formatted = []
+    decimal_map = {'0': 1, '083': 2, '166': 3, '25': 4, '333': 5, '416': 6, '5': 7, '583': 8, '666': 9, '75': 10, '833': 11, '916': 12}
+    for i in x_data:
+        remainder, year = math.modf(i)
+        if len(str(remainder)[2:]) <= 2:
+            month = decimal_map[str(remainder)[2:]]
+            x_data_formatted.append(dt.datetime(year=int(year), month=month, day=1))
+        else:
+            month = decimal_map[str(remainder)[2:5]]
+            x_data_formatted.append(dt.datetime(year=int(year), month=month, day=1))
 
-    x_data, y_data = process_data(df, plot_vars_map['process_type'], plot_vars_map['range'])
+    return mdates.num2date(x_data_formatted)
+
+
+def plot(ptype, df_list, plot_vars_map):
+
+    x_data_list = []
+    y_data_list = []
+    for df in df_list:
+        x_data, y_data = process_data(df, plot_vars_map['process_type'], plot_vars_map['begin_month'])
+        x_data_list.append(x_data)
+        y_data_list.append(y_data)
+
     if ptype == 'scatter':
         pass
     elif ptype == 'poly':
         pass
     elif ptype == 'poly_deriv':
-        plot_poly_deriv(x_data, y_data, plot_vars_map['degree'], plot_vars_map['deriv_degree'])
         return plot_poly_deriv(x_data_list, y_data_list, plot_vars_map['degree'], plot_vars_map['deriv_degree'], 
                                plot_vars_map['plots_per_graph'], plot_vars_map['counties'])
     elif ptype == 'scatter_poly':
-        scatter_poly(x_data, y_data, plot_vars_map['degree'])
+        return scatter_poly(x_data_list, y_data_list, plot_vars_map['degree'], plot_vars_map['plots_per_graph'], plot_vars_map['counties'])
     elif ptype == 'us_heatmap':
         pass
     else:
         return 'Invalid plot type!'
 
 
-def process_data(df, process_type, data_range):
+def process_data(df, process_type, beginMonth):
     x_data = []
     y_data = []
 
     if process_type == 'months':
         for i in df.iloc[:,0]:
-            for j in data_range:
-                x_data.append(int(str(i)[-4:]) + j / 12)
+            for j in range(df.shape[1]-1):
+                x_data.append(int(str(i)[-4:]) + (j + beginMonth) / 12)
 
         for i, row in df.iterrows():
-            for j in row[data_range.start+1:data_range.stop+1]:
+            for j in row[1:]:
                 y_data.append(j)
     return x_data, y_data
 
-def scatter_poly(x, y, deg):
+def scatter_poly(x, y, deg, plots_per_graph, counties):
     # Example of what coeffs and fiteq do, for a 3rd degree polynomial
     #d, c, b, a = poly.polyfit(x, y, 3)
     #fiteq = lambda x: a * x ** 3 + b * x ** 2 + c * x + d
-
-    coeffs = poly.polyfit(x, y, deg)
-    def fiteq(x, idx=0):
-        if idx == deg:
-            return coeffs[idx] * x ** (idx)
-        else:
-            return coeffs[idx] * x ** (idx) + fiteq(x, idx+1)
-
-    x_fit = np.array(x)
-    y_fit = fiteq(x_fit)
-
     fig, ax1 = plt.subplots()
-    lines = ax1.plot(x_fit, y_fit, color='r', alpha=0.5, label='Polynomial fit')
-    ax1.scatter(x, y, s=4, color='b', label='Data points')
-    ax1.set_title(f'Polynomial fit example deg={deg}')
+
+    colors = cm.rainbow(np.linspace(0, 1, len(counties)))
+    for x, y, county, color in zip(x, y, counties, colors):
+        coeffs = poly.polyfit(x, y, deg)
+        def fiteq(x, idx=0):
+            if idx == deg:
+                return coeffs[idx] * x ** (idx)
+            else:
+                return coeffs[idx] * x ** (idx) + fiteq(x, idx+1)
+
+        x_fit = np.array(x)
+        y_fit = fiteq(x_fit)
+
+        lines = ax1.plot(x_fit, y_fit, color=color, alpha=0.5, label=county)
+        ax1.scatter(x, y, s=4, color=color)
+
+    ax1.set_title(f'Polynomial fit deg={deg}')
     ax1.legend()
     #plt.subplots_adjust(right=0.8)
     #plt.table([['{:.10f}'.format(coeffs[x])[:9]] for x in range(len(coeffs)-1, -1, -1)], 
@@ -101,7 +133,8 @@ def scatter_poly(x, y, deg):
     #          colLabels=['Poly Coeffs'], loc='right', colWidths = [0.2])
     #plt.text(15, 3.4, 'Coefficients', size=12)
     cursor = mplcursors.cursor()
-    plt.show()
+    #plt.show()
+    return fig
 
 def plot_poly_deriv(x, y, deg, deriv_deg, plots_per_graph, counties):
     
@@ -127,26 +160,18 @@ def plot_poly_deriv(x, y, deg, deriv_deg, plots_per_graph, counties):
     cursor = mplcursors.cursor()
     return fig
 
+def tkinter_scatter_poly(x, y, deg):
+    coeffs = poly.polyfit(x, y, deg)
+    def fiteq(x, idx=0):
+        if idx == deg:
+            return coeffs[idx] * x ** (idx)
+        else:
+            return coeffs[idx] * x ** (idx) + fiteq(x, idx+1)
+
     x_fit = np.array(x)
     y_fit = fiteq(x_fit)
 
     fig, ax1 = plt.subplots()
-    lines = ax1.plot(x_fit, y_fit, color='r', alpha=0.5, label=f'Polynomial deg={deg}, Derivative d={deriv_deg}')
-    ax1.legend()
-    plt.show()
-
-def scatter_plot(x, y):
-    x_data = np.array(x)
-    y_data = np.array(y)
-
-    y_axis = np.arange(0, 120, 10)
-    x_axis = np.arange(x_data[0], x_data[-1], 2)
-
-    plt.figure(figsize=(20,5))
-    plt.xticks(x_axis)
-    plt.yticks(y_axis)
-    plt.scatter(x_data, y_data)
-    plt.show()
     lines = ax1.plot(x_fit, y_fit, color='r', alpha=0.5, label='Polynomial fit')
     scatter = ax1.scatter(x, y, s=4, color='b', label='Data points')
     ax1.set_title(f'Polynomial fit example deg={deg}')
