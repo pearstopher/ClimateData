c69fa69911fdf654353d8d7629ab86ef4482145a
ericanderson333 <ericsanderson333@gmail.com>
2022-05-17 15:44:08 -0700

    Shifted bottom data type drop down to align with other boxes

--- ClimateData/UI.py
+++ ClimateData/UI.py
@@ -602,7 +602,7 @@ class graphPage(tk.Frame):
             self.dropdown_graphs['state'] = 'readonly'
             self.dropdown_graphs['values'] = ["Minimum temperature", "Maximum temperature", "Average temperature", "Precipitation", "Palmer Drought Severity", "Palmer Hydrological Drought", "Modified Palmer Drought Severity", "1-month Standardized Precipitation", "2-month Standardized Precipitation", "3-month Standardized Precipitation", "6-month Standardized Precipitation", "9-month Standardized Precipitation", "12-month Standardized Precipitation", "24-month Standardized Precipitation"]
             self.dropdown_graphs.bind('<<ComboboxSelected>>', gen_datatype_columns)
-            self.dropdown_graphs.grid(row=8, column=1,  padx=(0, 200), pady=(40, 0))
+            self.dropdown_graphs.grid(row=8, column=1,  padx=(0, 190), pady=(30, 0))
             datatypeTip = Hovertip(self.dropdown_graphs, 'Select which type of weather data to graph')
 
 
